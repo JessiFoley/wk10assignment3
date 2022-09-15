424fb5de3643e848ace1bb4bc9caab1e75cf7183
Jessi Foley <jessifreyberger@gmail.com>
2022-09-15 15:52:46 -0500

    added a picture

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,15 +5,21 @@ const Home = props => {
   return (
     <home class="Home">
         <h1>Welcome to the Black Parade!</h1>
-        Ad deserunt eu ad ad elit nostrud quis fugiat officia minim eiusmod excepteur. Adipisicing eiusmod amet officia elit mollit. 
-        Irure duis consectetur aute sunt ad reprehenderit esse. Adipisicing eiusmod proident anim do Lorem voluptate dolore proident velit ullamco anim nulla pariatur. 
-        Consequat laborum laborum nostrud ullamco veniam exercitation consectetur in voluptate id magna eiusmod mollit.
+        <p>
+          Ad deserunt eu ad ad elit nostrud quis fugiat officia minim eiusmod excepteur. Adipisicing eiusmod amet officia elit mollit. 
+          rure duis consectetur aute sunt ad reprehenderit esse. Adipisicing eiusmod proident anim do Lorem voluptate dolore proident velit ullamco anim nulla pariatur. 
+          Consequat laborum laborum nostrud ullamco veniam exercitation consectetur in voluptate id magna eiusmod mollit.
+        </p>
+        <p>
         Dolor mollit exercitation enim mollit deserunt. Occaecat consequat eiusmod quis eu enim consectetur. 
         Id amet minim Lorem amet deserunt ex proident ullamco id commodo commodo cupidatat. Adipisicing do eu elit deserunt laborum officia ad tempor non dolor est. 
         Dolor irure qui sint sunt duis fugiat eu deserunt sint.
         Proident aliquip magna elit non in reprehenderit ipsum tempor anim consectetur deserunt in eu. Aliqua ullamco do eiusmod amet sint est dolor tempor. 
         Reprehenderit pariatur sunt labore consequat ad veniam voluptate. Culpa veniam ad excepteur eiusmod esse aliquip mollit eiusmod proident nisi est eiusmod tempor amet. 
         Cillum duis aliqua nostrud nisi.
+        </p>
+        <center><img src='vader_saber_bbq_fork.jpg' class='img-rounded' alt='BBQ Darth Vader' /></center>
+
     </home>
   )
 }
